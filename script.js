24fc8ee3e035fdfcc43d8335cedecee251df8f60
Howard <howardsoon@gmail.com>
2023-01-30 17:41:08 +0000

    complete  level 100

--- script.js
+++ script.js
@@ -6,7 +6,36 @@ function setup() {
 
 function makePageForEpisodes(episodeList) {
   const rootElem = document.getElementById("root");
-  rootElem.textContent = `Got ${episodeList.length} episode(s)`;
+  episodeList.forEach((episode) => {
+    const episodeElem = document.createElement("div");
+    episodeElem.classList.add("episode-card");
+
+    // Create title and episode number elements
+    const titleElem = document.createElement("h2");
+    titleElem.classList.add("episode-title");
+    titleElem.textContent = episode.name;
+
+    const episodeNumberElem = document.createElement("p");
+    episodeNumberElem.classList.add("episode-code");
+    episodeNumberElem.textContent = `S${("0" + episode.season).slice(-2)}E${(
+      "0" + episode.number
+    ).slice(-2)}`;
+
+    const titleWrapper = document.createElement("div");
+    titleWrapper.classList.add("title-wrapper");
+    titleWrapper.append(titleElem, episodeNumberElem);
+
+    const episodeImg = document.createElement("img");
+    episodeImg.src = episode.image.medium;
+
+    const episodeSummary = document.createElement("p");
+    episodeSummary.textContent = episode.summary;
+
+    episodeElem.append(titleWrapper, episodeImg, episodeSummary);
+    rootElem.appendChild(episodeElem);
+  });
 }
 
+
+// Call the setup function when the page is loaded
 window.onload = setup;
